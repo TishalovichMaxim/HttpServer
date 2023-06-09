f71d13e3296203af8e5d2799eb5c388a019836b3
Tishalovich Maxim <tishalovichm@gmailcom>
2023-06-09 22:43:06 +0300

    Http server template

---
+++ HTTPServer/HTTPServer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Http
+{
+    public class HttpServer
+    {
+        public string Host
+        {
+            get;
+            private set;
+        }
+
+        public int Port
+        {
+            get;
+            private set;
+        }
+
+        public HttpServer(string host, int port) 
+        {
+            this.Host = host;
+            this.Port = port;
+        }
+
+        public void Run()
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(this.Host), this.Port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(ipPoint);
+            socket.Listen(10);
+            Console.WriteLine("Waiting for connections...");
+
+            while (true)
+            {
+                Socket clientSocket = socket.Accept();
+                
+                string message = "Hello, world";
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                clientSocket.Send(data);
+
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+    }
+}
---
+++ HTTPServer/Program.cs
@@ -0,0 +1,4 @@
+﻿using Http;
+
+HttpServer httpServer = new HttpServer("127.0.0.1", 54_223);
+httpServer.Run();
